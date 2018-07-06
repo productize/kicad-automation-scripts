467651dcc713b8a21cffb933c6c83f5edf0e6d83
Bastiaan van der Peet <bastiaan@productize.be>
2018-07-06 15:07:14 +0200

    make sure plot always starts from same option

--- eeschema/export_schematic.py
+++ eeschema/export_schematic.py
@@ -90,7 +90,6 @@ def eeschema_plot_schematic(output_directory, file_format, all_pages):
 
     if not all_pages:   # all pages is default option
         command_list.extend(['Tab', 'Tab', 'Tab', 'Tab'])
-    print(command_list)
     xdotool(command_list)
 
     logger.info('Plot')
@@ -98,6 +97,25 @@ def eeschema_plot_schematic(output_directory, file_format, all_pages):
 
     logger.info('Wait before shutdown')
     time.sleep(2)
+
+def set_default_plot_option():
+    # eeschema saves the latest plot format, this is problematic because
+    # plot_schematic() does not know which option is set (it assumes HPGL)
+    opt_file_path = os.path.expanduser('~/.config/kicad/')
+    in_p = os.path.join(opt_file_path, 'eeschema')
+    out_p = os.path.join(opt_file_path, 'eeschema.new')
+    in_f = open(in_p)
+    out_f = open(out_p, 'w')
+    for in_line in in_f:
+        param, value = in_line.split('=', 1)
+        if param == 'PlotFormat':
+            out_line = 'PlotFormat=0\n'  # 1: ps, 4: pdf, 5:svg, 3: dxf, 0: hpgl
+        else:
+            out_line = in_line
+        out_f.write(out_line)
+    out_f.close()
+    os.remove(in_p)
+    os.rename(out_p, in_p)
 
 def export_schematic(schematic, output_dir, file_format, all_pages):
     file_util.mkdir_p(output_dir)
@@ -107,6 +125,7 @@ def export_schematic(schematic, output_dir, file_format, all_pages):
     schematic_output_png_file = os.path.join(output_dir, 'schematic.png')
 
     with recorded_xvfb(screencast_output_file, width=800, height=600, colordepth=24):
+        set_default_plot_option()
         with PopenContext(['eeschema', schematic], close_fds=True) as eeschema_proc:
             eeschema_plot_schematic(output_dir, file_format, all_pages)
             eeschema_proc.terminate()
