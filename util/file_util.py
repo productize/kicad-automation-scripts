be8264080551e4e58ef7bc4a5e203fc3a25125d0
Seppe Stas <seppe@productize.be>
2019-04-23 20:19:16 +0200

    Fix not checking for open files correctly

--- util/file_util.py
+++ util/file_util.py
@@ -21,17 +21,22 @@ def mkdir_p(path):
 def wait_for_file_created_by_process(pid, file, timeout=5):
     process = psutil.Process(pid)
 
-    DELAY = 0.05
+    DELAY = 0.01
     for i in range(int(timeout/DELAY)):
+        open_files = process.open_files()
+        logger.debug(open_files)
         if os.path.isfile(file):
-            if file in process.open_files():
+            file_open = False
+            for open_file in open_files:
+                if open_file.path == file:
+                    file_open = True
+            
+            if file_open:
                 logger.debug('Waiting for process to close file')
-                pass
             else:
                 return
         else:
             logger.debug('Waiting for process to create file')
-            pass
         time.sleep(DELAY)
 
     raise RuntimeError('Timed out waiting for creation of %s' % file)
